6240ca353015cc72fa206080437b1c636baee2e0
gelargew <gelargew@gmail.com>
2021-07-26 18:52:21 +0700

    filename bug

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -2,8 +2,6 @@ import { Box, OrbitControls } from '@react-three/drei'
 import React, { Suspense } from 'react'
 import Layout from '../components/Layout'
 import { Canvas } from '@react-three/fiber'
-import Kaonashi from '../components/kaonashi'
-
 
 export default function Room() {
 
@@ -28,7 +26,6 @@ const RoomMesh = () => {
             <Box args={[10, 5, 5, 10, 5, 5]} >
                 <meshPhongMaterial wireframe />
                 <ambientLight intensity={2} />
-                <Kaonashi />
             </Box>
         </mesh>
     )
